c6ecdd1664cac1ea595b0c1e5f70885c05efb5c0
SirapopNaja <goon63140@gmail.com>
2020-01-08 14:47:26 +0700

    get ข้อมูลตามระดับปริญญา

--- app/Http/Controllers/grauateController.php
+++ app/Http/Controllers/grauateController.php
@@ -16,19 +16,22 @@ class grauateController extends Controller
 
     public function seclectgrauate01() {
         // จำนวนนิสิตที่อยู่ในหลักสูตร
-        $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_EN,COUNT(VW_VOQ_COURSE.COURSE_NAME_EN)
-                            FROM VW_VOQ_STD_GRADUATE
+       $DEGREE_CODE =  (isset($_GET['degree']))?$_GET['degree']:3;
+       $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_EN,COUNT(VW_VOQ_COURSE.COURSE_NAME_EN) as COUNT_STUDENT
+                            FROM VW_VOQ_STD_GRADUATE 
                             JOIN VW_VOQ_COURSE ON VW_VOQ_STD_GRADUATE.COURSE_ID=VW_VOQ_COURSE.COURSE_ID
-                            GROUP BY COURSE_NAME_EN ');
-                return $result1 ;
+                            WHERE VW_VOQ_STD_GRADUATE.DEGREE_CODE = '.$DEGREE_CODE.'
+                            GROUP BY COURSE_NAME_EN'); 
+
+                return  view('Home',["course"=>$result1,'degree'=>$DEGREE_CODE]);
     }
 
-    public function seclectgrauate02() {
+        public function seclectgrauate02() {
         $result = DB::table('VW_VOQ_STD_GRADUATE')
         ->join('VW_VOQ_COURSE','VW_VOQ_STD_GRADUATE.COURSE_NAME_TH', '=', 'VW_VOQ_STD_GRADUATE.COURSE_ID')
         ->select('VW_VOQ_COURSE.COURSE_ID','VW_VOQ_STD_GRADUATE.COURSE_NAME_TH')
         ->get();
-        return $result;
+        return  view('Home',[""=>$result]);
     }
     
     
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,55 +1,37 @@
-<!DOCTYPE html>
+ 
+
+
+
+ <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.dropbtn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown-content a:hover {background-color: #ddd;}
-
-.dropdown:hover .dropdown-content {display: block;}
-
-.dropdown:hover .dropbtn {background-color: #3e8e41;}
-</style>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<h2>test</h2>
-<p>เทสเลือกระดับปริญญา</p>
-
-<div class="dropdown">
-  <button class="dropbtn">ระดับปริญญา</button>
-  <div class="dropdown-content">
-    <a href="/db2">ตรี</a>
-    <a href="/db1">โท</a>
-    <a href="#">เอก</a>
+<div class="container">
+  <h2>Table</h2>
+  <p>The .table-responsive class creates a responsive table which will scroll horizontally on small devices (under 768px). When viewing on anything larger than 768px wide, there is no difference:</p>                                                                                      
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr> 
+        <th>Name</th>
+        <th>Count</th> 
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($course as $val) 
+      <tr>
+        <td>{{ $val->COURSE_NAME_EN }}</td>
+        <td>{{ $val->COUNT_STUDENT }}</td> 
+      </tr>
+       @endforeach 
+    </tbody>
+  </table>
   </div>
 </div>
 
