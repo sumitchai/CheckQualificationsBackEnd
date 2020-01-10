a71f0cd1834ea1a767bf5402e3916fdf52ff7515
sumitchai <50207852+sumitchai@users.noreply.github.com>
2020-01-10 16:27:37 +0700

    add หน้า

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,74 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <!-- Site made with Mobirise Website Builder v4.11.6, https://mobirise.com -->
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="generator" content="Mobirise v4.11.6, mobirise.com">
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-  <link rel="shortcut icon" href="assets/images/up-122x122.png" type="image/x-icon">
-  <meta name="description" content="">
-  
-  <title>Home</title>
-  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-  <link rel="stylesheet" href="assets/tether/tether.min.css">
-  <link rel="stylesheet" href="assets/dropdown/css/style.css">
-  <link rel="stylesheet" href="assets/theme/css/style.css">
-  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
-  
-  
-  
-</head>
-<body>
-  <section class="menu cid-rL4N5C6fZy" once="menu" id="menu2-23">
+@extends('layout.header')
 
-    
-
-    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <div class="hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </button>
-        <div class="menu-logo">
-            <div class="navbar-brand">
-                <span class="navbar-logo">
-                    <a href="https://mobirise.co">
-                        <img src="assets/images/up-122x122.png" alt="Mobirise" title="" style="height: 3.8rem;">
-                    </a>
-                </span>
-                
-            </div>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item dropdown">
-                    <a class="nav-link link text-black dropdown-toggle display-4" href="https://mobirise.co" data-toggle="dropdown-submenu" aria-expanded="false">เมนู</a><div class="dropdown-menu"><a class="text-black dropdown-item display-4" href="index.html">หน้าหลัก</a><a class="text-black dropdown-item display-4" href="CheckCourse.html">ตรวจสอบคุณวุฒิตามหลักสูตร</a><a class="text-black dropdown-item display-4" href="Checkindividual.html">ตรวจสอบคุณวุฒิรายบุคคล</a><a class="text-black dropdown-item display-4" href="studying.html">รายชื่อนิสิตชาวต่างชาติที่กำลังศึกษา</a></div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link link text-black display-4" href="https://mobirise.co" aria-expanded="true">ภาษาไทย</a>
-              </li>
-              <li class="nav-item">
-                <a class="text display-4">|</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link link text-black display-4" href="https://mobirise.co" aria-expanded="true">English</a>
-              </li></ul>
-            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-success display-4" href="tel:+1-234-567-8901">
-                <span class="mobi-mbri mobi-mbri-phone mbr-iconfont mbr-iconfont-btn"></span>
-                    <font face="MobiriseIcons"><span style="font-size: 25.6px;">ติดต่อเรา</span></font>
-                &nbsp;
-              </a></div>
-        </div>
-    </nav>
-</section>
-
-<section class="engine"><a href="https://mobirise.info/u">bootstrap responsive templates</a></section><section class="header5 cid-rL4naKPYLJ mbr-parallax-background" id="header5-22">
+@section('main')
+<br><br><br><br>
+<section class="engine"><a href="https://mobirise.info/l">free site templates</a></section><section class="header5 cid-rN35s1IUnO mbr-parallax-background" id="header5-24">
 
     
 
@@ -81,40 +15,12 @@
             <div class="text-content">
                 <h1 class="mbr-section-subtitle mbr-light pb-3 mbr-fonts-style display-1">ตรวจสอบคุณวุฒิ<br>มหาวิทยาลัยพะเยา</h1><br>
              
-                <div class="mbr-section-btn"><a class="btn btn-md btn-bgr btn-primary display-4" href="CheckCourse.html">ตรวจสอบคุณวุฒิตามหลักสูตร</a> <a class="btn btn-md btn-bgr btn-primary display-4" href="Checkindividual.html">ตรวจสอบคุณวุฒิรายบุคคล</a> <a class="btn btn-md btn-bgr btn-primary display-4" href="studying.html">รายชื่อนิสิตชาวต่างชาติที่กำลังศึกษา</a></div>
+                <div class="mbr-section-btn">
+                    <a class="btn btn-md btn-bgr btn-primary display-4" href="CheckCourse.html">ตรวจสอบคุณวุฒิตามหลักสูตร</a> 
+                    <a class="btn btn-md btn-bgr btn-primary display-4" href="Checkindividual.html">ตรวจสอบคุณวุฒิรายบุคคล</a> 
+                    
+                </div>
             </div>
         </div>
     </div>
 </section>
-
-<section once="footers" class="cid-rN2KlCKV3L" id="footer6-24">
-
-    
-
-    
-
-    <div class="container">
-        <div class="media-container-row align-center mbr-white">
-            <div class="col-12">
-                <p class="mbr-text mb-0 mbr-fonts-style display-7">มหาวิทยาลัยพะเยา, ระบบตรวจสอบคุณวุฒิ
-<br>Copyright 2019 © University of Phayao Page</p>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-  <script src="assets/web/assets/jquery/jquery.min.js"></script>
-  <script src="assets/popper/popper.min.js"></script>
-  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/tether/tether.min.js"></script>
-  <script src="assets/smoothscroll/smooth-scroll.js"></script>
-  <script src="assets/dropdown/js/nav-dropdown.js"></script>
-  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
-  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
-  <script src="assets/parallax/jarallax.min.js"></script>
-  <script src="assets/theme/js/script.js"></script>
-  
-  
-</body>
-</html>
