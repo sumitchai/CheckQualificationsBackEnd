523446ec8099a6fd12b4e1c41a6b95857c9f9c5b
sumitchai <50207852+sumitchai@users.noreply.github.com>
2020-01-13 14:56:28 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,8 @@
             <div class="text-content">
                 <h1 class="mbr-section-subtitle mbr-light pb-3 mbr-fonts-style display-1">ตรวจสอบคุณวุฒิ<br>มหาวิทยาลัยพะเยา</h1><br>
              
-                <div class="mbr-section-btn"><a class="btn btn-md btn-bgr btn-primary display-4" href="CheckCourse.html">ตรวจสอบคุณวุฒิตามหลักสูตร</a> <a class="btn btn-md btn-bgr btn-primary display-4" href="Checkindividual.html">ตรวจสอบคุณวุฒิรายบุคคล</a> <a class="btn btn-md btn-bgr btn-primary display-4" href="studying.html">รายชื่อนิสิตชาวต่างชาติที่กำลังศึกษา</a></div>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-bgr btn-primary display-4" href="/SelectTH">ตรวจสอบคุณวุฒิตามหลักสูตร</a> 
+                    <a class="btn btn-md btn-bgr btn-primary display-4" href="/Checkindividual">ตรวจสอบคุณวุฒิรายบุคคล</a></div>
             </div>
         </div>
     </div>
