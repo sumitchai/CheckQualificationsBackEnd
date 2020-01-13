29f3a5d8e3ec566770a24f9531a1fb99353983f9
SirapopNaja <goon63140@gmail.com>
2020-01-13 09:44:45 +0700

    ลำดับที่

--- app/Http/Controllers/grauateController.php
+++ app/Http/Controllers/grauateController.php
@@ -9,7 +9,7 @@ class grauateController extends Controller
 {
     public function seclectgrauate() {
         $DEGREE_CODE =  (isset($_GET['degree']))?$_GET['degree']:3;
-        $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_TH,COUNT(VW_VOQ_COURSE.COURSE_NAME_TH) as COUNT_STUDENT
+        $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_TH,VW_VOQ_COURSE.COURSE_ID,COUNT(VW_VOQ_COURSE.COURSE_NAME_TH) as COUNT_STUDENT
                              FROM VW_VOQ_STD_GRADUATE 
                              JOIN VW_VOQ_COURSE ON VW_VOQ_STD_GRADUATE.COURSE_ID=VW_VOQ_COURSE.COURSE_ID
                              WHERE VW_VOQ_STD_GRADUATE.DEGREE_CODE = '.$DEGREE_CODE.'
@@ -21,7 +21,7 @@ class grauateController extends Controller
     public function seclectgrauate01() {
         // จำนวนนิสิตที่อยู่ในหลักสูตร
        $DEGREE_CODE =  (isset($_GET['degree']))?$_GET['degree']:3;
-       $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_EN,COUNT(VW_VOQ_COURSE.COURSE_NAME_EN) as COUNT_STUDENT
+       $result1=DB::select('SELECT VW_VOQ_COURSE.COURSE_NAME_EN,VW_VOQ_COURSE.COURSE_ID,COUNT(VW_VOQ_COURSE.COURSE_NAME_EN) as COUNT_STUDENT
                             FROM VW_VOQ_STD_GRADUATE 
                             JOIN VW_VOQ_COURSE ON VW_VOQ_STD_GRADUATE.COURSE_ID=VW_VOQ_COURSE.COURSE_ID
                             WHERE VW_VOQ_STD_GRADUATE.DEGREE_CODE = '.$DEGREE_CODE.'
