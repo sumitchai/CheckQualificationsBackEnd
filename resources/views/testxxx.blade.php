b0004e1beeadd0c3b0ffa8a54eef699cd302bcb4
sumitchai <50207852+sumitchai@users.noreply.github.com>
2020-01-17 11:25:58 +0700

    ...

--- resources/views/testxxx.blade.php
+++
@@ -1,5 +0,0 @@
-<select name="forma" onchange="location = this.options[this.selectedIndex].value;">
-    <option value="/SelectENlist">Home</option>
-    <option value="Contact.php">Contact</option>
-    <option value="Sitemap.php">Sitemap</option>
-</select>
