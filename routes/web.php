d76472f29f4c5998a81bd91e32de7d5636301f1a
yukisoru <33993605+yukisoru@users.noreply.github.com>
2020-01-17 11:22:23 +0700

    ...

--- routes/web.php
+++ routes/web.php
@@ -34,10 +34,6 @@ Route::get('/SelectENlist', function () {
     return view('SelectENlist');
 });
 
-Route::get('/testxxx', function () {
-    return view('testxxx');
-});
-
 Route::get ('/Check_course','grauateController@seclectgrauate');
 
 Route::get ('/SelectThailist','grauateController@seclectcourse');
