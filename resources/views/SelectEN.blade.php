5f8f0d0cea4154d429562587511113f1537adeb3
yukisoru <33993605+yukisoru@users.noreply.github.com>
2020-01-27 09:28:03 +0700

    ...

--- resources/views/SelectEN.blade.php
+++ resources/views/SelectEN.blade.php
@@ -1,24 +1,13 @@
+@extends('layout.header')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Qualification | University Of Phayao</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-</head>
-<body>
-
-<div class="container container-table">
+@section('main')
+<br><br><br><br>
+<section class="section-table cid-rN9ViYOSs1" id="table1-2e">
+  <div class="container container-table">
       <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-5">Check the qualifications according to the course<br></h2>
       <h3 class="mbr-section-subtitle mbr-fonts-style align-center pb-5 mbr-light display-5">
-        <strong>Education level : <!---ปริญญา----> </strong><br><br>
-        <strong> <!-----ชื่อหลักสูตร------>
+        <strong>Education level : <!---ปริญญา----></strong><br>
+        <strong> <!-----ชื่อหลักสูตร------><strong>
       </h3>
       <div class="table-wrapper">
         <div class="container">
@@ -26,14 +15,13 @@
             <div class="col-md-6"></div>
             <div class="col-md-6">
                 <div class="dataTables_filter">
-                  <label class="searchInfo mbr-fonts-style display-6">search:</label>
-                  <input class="form-control input-sm" disabled=""><br>
+                  <label class="searchInfo mbr-fonts-style display-6">search: </label>
+                  <input class="form-control input-sm" disabled="">
                 </div>
             </div>
           </div>
         </div>
-     
- 
+
         <div class="container scroll">
           <table class="table isSearch" cellspacing="0">
             <thead>
@@ -41,32 +29,31 @@
               <th class="head-item mbr-fonts-style display-6"> No </th>
               <th class="head-item mbr-fonts-style display-6">StudentID </th>
               <th class="head-item mbr-fonts-style display-6">Name</th>
-              
               <th class="head-item mbr-fonts-style display-6">ACAD_YEAR</th>
               </tr>
             </thead>
+            
             <tbody>
               <?php $i = 1; ?>
               @foreach ($data as $x)
-            <tr> 
+              <tr> 
               <td class="body-item mbr-fonts-style display-6">{{$i++}}</td>
               <td class="body-item mbr-fonts-style display-6">{{$x->STUDENT_CODE}}</td>
               <td class="body-item mbr-fonts-style display-6">{{$x->NAME_EN}}</td>
               
               <td class="body-item mbr-fonts-style display-6">{{$x->ACAD_YEAR}}</td>
               </tr>
-            @endforeach
+              @endforeach
               </tbody>
-            
           </table>
         </div>
         <div class="container table-info-container">
           <div class="row info">
             <div class="col-md-6">
               <div class="dataTables_info mbr-fonts-style display-7">
-                <span class="infoBefore">{{__('text.sh')}}</span>
+                <span class="infoBefore">Total</span>
                 <span class="inactive infoRows"></span>
-                <span class="infoAfter">{{__('text.sh2')}}</span>
+                <span class="infoAfter">COURSE</span>
                 <span class="infoFilteredBefore">(filtered from</span>
                 <span class="inactive infoRows"></span>
                 <span class="infoFilteredAfter"> total entries)</span>
@@ -78,9 +65,8 @@
       </div>
     </div>
 </section>
-</body>
-</html>
-<section class="section-table cid-rN9ViYOSs1" id="table1-2e">
+
+
 
   
   
