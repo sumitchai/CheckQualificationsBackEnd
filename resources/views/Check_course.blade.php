1f6ecc9e73c1a340011e81d94ebd0846f14004cd
SirapopNaja <goon63140@gmail.com>
2020-03-02 14:35:05 +0700

    ...

--- resources/views/Check_course.blade.php
+++ resources/views/Check_course.blade.php
@@ -1,7 +1,133 @@
-@extends('layout.header')
 
-@section('main')
-<br><br><br><br>
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.11.6, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.11.6, mobirise.com">
+
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="{{env('APP_URL')}}/assets/images/up-536x536.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Qualification | University Of Phayao</title>
+  <!-----fonts--->
+  <link href="https://fonts.googleapis.com/css?family=K2D&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/web/{{env('APP_URL')}}/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/datatables/data-tables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{env('APP_URL')}}/assets/theme/css/style.css">
+  <link rel="preload" as="style" href="{{env('APP_URL')}}/assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="{{env('APP_URL')}}/assets/mobirise/css/mbr-additional.css" type="text/css">
+ 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+  
+  
+</head>
+ 
+<body>
+     
+<section class="menu cid-rN35OUwakP" once="menu" id="menu2-26">
+ 
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="/">
+                        <img src="{{env('APP_URL')}}/assets/images/up-536x536.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item dropdown">
+                    <a class="nav-link link text-black dropdown-toggle display-4" href="https://mobirise.co" data-toggle="dropdown-submenu" aria-expanded="true">เมนู</a>
+              <div class="dropdown-menu">
+                <a class="text-black dropdown-item display-4" href="/">หน้าแรก</a>
+                <a class="text-black dropdown-item display-4" href="/Check_course">ตรวจสอบคุณวุฒิตามหลักสูตร </a>
+                <a class="text-black dropdown-item display-4" href="/Checkindividual">ตรวจสอบคุณวุฒิรายบุคคล</a>
+                
+              </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link link text-black display-4" href="/Check_course" aria-expanded="false">ภาษาไทย</a>
+              </li>
+              <li class="nav-item">
+                <a class="text display-4">|</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link link text-black display-4" href="/Check_course_EN" aria-expanded="false">English</a>
+              </li></ul>
+            <div class="navbar-buttons mbr-section-btn">
+                <a class="btn btn-sm btn-success display-4"  data-toggle="modal" data-target="#myModal">
+                <span class="display-4 mobi-mbri mobi-mbri-phone mbr-iconfont mbr-iconfont-btn"></span>
+                    <font face="MobiriseIcons">
+                        <span class="display-3">ติดต่อ</span>
+                    </font>
+                &nbsp;
+              </a>
+            </div>
+        </div>
+    </nav>
+    <!-- Modal -->
+<div class="modal fade" id="myModal" role="dialog" style="height:550px;">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-center" id="myModalLong">กองบริการการศึกษา : มหาวิทยาลัยพะเยา</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+            </div>
+            <div class="display-4 modal-body text-center" style="font-size: 18px;">
+                <b>งานทะเบียนนิสิตและประมวลผล</b><br>
+                <a>19 หมู่ 2 ตำบลแม่กา อำเภอเมือง จังหวัดพะเยา รหัสไปรษณีย์ 56000</a><br>
+                <a>โทรศัพท์:  054-466666 ต่อ 1021 </a>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+            </div><br/>
+        </div>
+    </div>
+</div>
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+    <!-- Copyright -->
+    <div class="display-4 bg-dark text-white-50 page-footer text-center ">
+        <br>
+        <br>
+        มหาวิทยาลัยพะเยา, ระบบตรวจสอบคุณวุฒิ<br>
+        Copyright 2019 © University of Phayao Page
+        <br>
+        <br>
+        <div>
+            <a href="home" class="display-4 float-right">แก้ไขเว็บไซต์ </a><br>
+        </div>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</section>
 <section class="section-table cid-rN9ViYOSs1" id="table1-2e">
   <div class="container container-table">
       <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-5">ตรวจสอบคุณวุฒิตามหลักสูตร <br></h2>
@@ -92,5 +218,36 @@
       </div>
     </div>
 </section>
+        
+
+
+  <script src="{{env('APP_URL')}}/assets/web/{{env('APP_URL')}}/assets/jquery/jquery.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/popper/popper.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="{{env('APP_URL')}}/assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="{{env('APP_URL')}}/assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="{{env('APP_URL')}}/assets/tether/tether.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/parallax/jarallax.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/datatables/jquery.data-tables.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/datatables/data-tables.bootstrap4.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/theme/js/script.js"></script>
+  <script src="{{env('APP_URL')}}/assets/formoid/formoid.min.js"></script>
+  <script>
+  $(window).load(function(){
+    $('#cover').fadeOut(1000);
+    
+});
+  </script>
+  <script>
+    function goBack() {
+      window.history.back();
+    }
+    </script>
+  
+</body>
+</html> 
+
 
 
